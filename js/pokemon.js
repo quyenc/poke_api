81667405b0d0cc61f43468a9e1ae0b602a7305b3
Quyen Castellanos <quyencastellanos@QCASTELLANOS-MBP-DAL.local.tld>
2016-08-18 14:57:23 -0500

    Pushing pokemon constructor but images not loading for some reason

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,20 +1,38 @@
-// Will want to pass in a list of numbers that the pokemon CANNOT be
-function getRandomPokemon(restrictedIds, isOpponent){    
-	var num = Math.floor((Math.random() * 150) + 1);
-	while (restrictedIds.includes(num)) {
-		num = Math.floor((Math.random() * 150) + 1);
-	}
-	var url = "http://pokeapi.co/api/v2/pokemon/" + num + "/";
+var usedPokemon = [];
+
+var Pokemon = function (id) {
+	var url = "http://pokeapi.co/api/v2/pokemon/" + id + "/";
 	$.get(url, function(res) {
-		console.log(res);
+		// Construct the Pokemon object
 		var name = res["name"];
 		console.log("name = " + name);
-		var imageString = res["sprites"].front_default;
-		console.log("image = " + imageString);
-		if (isOpponent) {
-			document.getElementById("opponent-pokemon-image").src = imageString;
-		} else {
-			document.getElementById("your-pokemon-image").src = imageString;
-		}
+		console.log(res);
+		var imageUrl = res["sprites"].front_default;
+		console.log(imageUrl);
+
+		this.id = id;
+		this.name = name;
+		this.imageUrl = imageUrl;
+
+		return this;
 	});
+}
+
+// Will want to pass in a list of numbers that the pokemon CANNOT be
+function getRandomPokemon(isOpponent){    
+	var num = Math.floor((Math.random() * 150) + 1);
+	while (usedPokemon.includes(num)) {
+		num = Math.floor((Math.random() * 150) + 1);
+	}
+
+	usedPokemon.push(num);
+	var randomPokemon = new Pokemon(num);
+
+	if (isOpponent) {
+		document.getElementById("opponent-pokemon-image").src = randomPokemon.imageUrl;
+	} else {
+		document.getElementById("your-pokemon-image").src = randomPokemon.imageUrl;
+	}
+
+	return randomPokemon;
 }
