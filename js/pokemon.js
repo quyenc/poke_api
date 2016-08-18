f6676ef76f378975ac7250a5356203f5a312556c
Quyen Castellanos <quyencastellanos@QCASTELLANOS-MBP-DAL.local.tld>
2016-08-18 14:19:19 -0500

    Random generated pokemon images now implemented

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,12 +1,20 @@
 // Will want to pass in a list of numbers that the pokemon CANNOT be
-function getRandomPokemon(restrictedIds){    
+function getRandomPokemon(restrictedIds, isOpponent){    
 	var num = Math.floor((Math.random() * 150) + 1);
 	while (restrictedIds.includes(num)) {
 		num = Math.floor((Math.random() * 150) + 1);
 	}
 	var url = "http://pokeapi.co/api/v2/pokemon/" + num + "/";
 	$.get(url, function(res) {
+		console.log(res);
 		var name = res["name"];
 		console.log("name = " + name);
+		var imageString = res["sprites"].front_default;
+		console.log("image = " + imageString);
+		if (isOpponent) {
+			document.getElementById("opponent-pokemon-image").src = imageString;
+		} else {
+			document.getElementById("your-pokemon-image").src = imageString;
+		}
 	});
 }
