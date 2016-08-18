f27aca5f02d7b52787cd5c75c0401910012ab8a1
Quyen Castellanos <quyencastellanos@QCASTELLANOS-MBP-DAL.local.tld>
2016-08-18 13:51:17 -0500

    Random pokemon generator

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,3 +1,7 @@
-function getRandomPokemon () {
-	console.log('getRandomPokemon');
-};
+function getRandomPokemon(){    
+	$.get("http://pokeapi.co/api/v2/pokemon/3/", function(res) {
+		var name = res["name"];
+		var num = Math.floor((Math.random() * 150) + 1);
+		console.log("name = " + name);
+	});
+}
