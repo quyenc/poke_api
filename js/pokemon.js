ffa0110d2089dea249c41b5d74b8554228bf7b4f
Quyen Castellanos <quyencastellanos@QCASTELLANOS-MBP-DAL.local.tld>
2016-08-18 13:59:22 -0500

    Random pokemon logic now avoids reusing pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,6 +1,9 @@
 // Will want to pass in a list of numbers that the pokemon CANNOT be
-function getRandomPokemon(){    
+function getRandomPokemon(restrictedIds){    
 	var num = Math.floor((Math.random() * 150) + 1);
+	while (restrictedIds.includes(num)) {
+		num = Math.floor((Math.random() * 150) + 1);
+	}
 	var url = "http://pokeapi.co/api/v2/pokemon/" + num + "/";
 	$.get(url, function(res) {
 		var name = res["name"];
