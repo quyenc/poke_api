14db8ec8d6f1f0d1174ccf317c65a7e6ef95900a
Dylan Parmley <dylanparmley@DPARMLEY-MBP-DAL.local.tld>
2016-08-19 09:13:14 -0500

    added moves

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,5 +1,49 @@
 var usedPokemon = [];
 
+randomInterval = function(min, max)
+{
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var Move = function(url, cb) {
+
+cb = cb || function(){};
+		  
+$.get(url, (function(res) {
+
+  if (!res) {
+    res = {
+      power: 0,
+      class: 'physical',
+      accuracy: 100
+    };
+  }
+
+  this.power = res.power;
+  this.class = res.type.name;
+  this.accuracy = res.accuracy;
+
+  cb(this);
+	}).bind(this));
+};
+
+function getMoves(data){
+    var moves=[];
+    var temp;
+
+
+    
+    while(moves.length<4)
+    {
+      temp =randomInterval(0, data.length-1);
+      moves.push(new Move(data[temp].move.url));
+    }
+    
+     
+     return moves; 
+    
+
+}
 var Pokemon = function (id, cb) {
 	cb = cb || function (){};
 	var url = "http://pokeapi.co/api/v2/pokemon/" + id + "/";
@@ -14,6 +58,7 @@ var Pokemon = function (id, cb) {
 		this.id = id;
 		this.name = name;
 		this.imageUrl = imageUrl;
+		this.moves=getMoves(res.moves);
 
 		cb(this);
 	}).bind(this));
@@ -38,5 +83,6 @@ function getRandomPokemon(isOpponent, cb){    
 
 		//cb(pokemon);
 	});
+	alert(); 
 
 }
